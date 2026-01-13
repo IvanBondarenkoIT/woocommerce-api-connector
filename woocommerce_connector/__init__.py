ee3f1a0f6b1dcf12ec430f93912bda5a3717f51f
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2026-01-13 11:00:05 +0400

    Fix imports in config module and main __init__
    - Fix relative import in config/__init__.py
    - Comment out GUI import in main __init__ until refactoring
    - Test imports work correctly

--- woocommerce_connector/__init__.py
+++ woocommerce_connector/__init__.py
@@ -6,7 +6,8 @@ and managing products with a modern GUI interface.
 """
 
 from .connector import WooCommerceConnector, check_api_version_standalone
-from .gui import WooCommerceGUI
+# GUI будет добавлен после рефакторинга
+# from .gui import WooCommerceGUI
 
 __version__ = "1.0.0"
 __author__ = "Ivan Bondarenko"
@@ -14,7 +15,9 @@ __email__ = ""
 
 __all__ = [
     "WooCommerceConnector",
-    "WooCommerceGUI",
     "check_api_version_standalone",
+    # "WooCommerceGUI",  # Будет добавлен после рефакторинга GUI
 ]
+
+
 
--- woocommerce_connector/config/__init__.py
+++ woocommerce_connector/config/__init__.py
@@ -4,6 +4,8 @@
 Содержит классы и константы для настройки приложения.
 """
 
+# Импортируем WooCommerceConfig из родительского модуля
+# config.py находится в woocommerce_connector/, а мы в woocommerce_connector/config/
 from ..config import WooCommerceConfig
 from .constants import (
     APIConstants,
